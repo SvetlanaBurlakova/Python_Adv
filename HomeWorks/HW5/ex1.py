275f3a4c464293601c59b37b62d0bd46ce4b2072
Svetlana Burlakova <svetlya4ok83@gmail.com>
2024-04-16 16:16:28 +0400

    HW5

---
+++ HomeWorks/HW5/ex1.py
@@ -0,0 +1,17 @@
+"""
+Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. Функция возвращает кортеж
+из трёх элементов: путь, имя файла, расширение файла.
+"""
+from os import path
+
+def parse_path(path_str: str) -> tuple:
+    "Функция возвращает путь, имя файла и расширение по переданному пути"
+    full_name = path.basename(path_str)
+    file_name = path.splitext(full_name)[0]
+    extention = path.splitext(full_name)[1]
+    return path_str, file_name, extention
+
+file_info = parse_path(r'C:\Users\OneDrive\Рабочий стол\GB\Python\HW.py')
+print(f'Путь: {file_info[0]}')
+print(f'Имя файла: {file_info[1]}')
+print(f'Расширение файла: {file_info[2]}')
---
+++ HomeWorks/HW5/ex3.py
@@ -0,0 +1,21 @@
+"""
+Создайте функцию генератор чисел Фибоначчи
+"""
+def fibonacci(n = 0, n1 = 0, n2 = 1):
+    """Функция генератор чисел Фибоначчи"""
+    while True:
+        if n == 0:
+            n += 1
+            yield n1
+        if n == 1:
+            n += 1
+            yield n2
+        n1, n2 = n2, n1 + n2
+        yield n2
+
+
+n = int(input('введите кол-во чисел  Фибоначчи: '))
+
+f = fibonacci()
+for _ in range(n):
+    print(next(f), end=' ')
