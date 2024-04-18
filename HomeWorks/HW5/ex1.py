42516f898f63333e2cfca5b8c28cbfc8d361b9c0
Svetlana Burlakova <svetlya4ok83@gmail.com>
2024-04-18 19:42:47 +0400

    HW5

--- HomeWorks/HW5/ex1.py
+++ HomeWorks/HW5/ex1.py
@@ -6,10 +6,11 @@ from os import path
 
 def parse_path(path_str: str) -> tuple:
     "Функция возвращает путь, имя файла и расширение по переданному пути"
+    path_to_file = path.dirname(path_str)
     full_name = path.basename(path_str)
     file_name = path.splitext(full_name)[0]
     extention = path.splitext(full_name)[1]
-    return path_str, file_name, extention
+    return path_to_file, file_name, extention
 
 file_info = parse_path(r'C:\Users\OneDrive\Рабочий стол\GB\Python\HW.py')
 print(f'Путь: {file_info[0]}')
