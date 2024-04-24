4a2dd6570553a8e890b180928321def35a9eaf4d
Svetlana Burlakova <svetlya4ok83@gmail.com>
2024-04-24 12:32:52 +0400

    HW7 - module

--- HomeWorks/HW7/__init__.py
+++ HomeWorks/HW7/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['ex1']
+from Seminars.Seminars7 import ex1, ex2, ex3, ex4, ex5, ex7
+from .hw_ex1 import group_files_rename
+
+__all__ = ['ex1', 'ex2', 'ex3', 'ex4', 'ex5', 'ex7', 'hw_ex1']
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # from Seminars.Seminar6 import ex2
 from Seminars.Seminar6 import guess_number
+from HomeWorks import HW7
+from pathlib import Path
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # guess_number()
+    HW7.ex7.sort_files(Path('dir'))
+    HW7.group_files_rename(Path('dir'), end_file='_file_',digits=3,
+                       extention_before='rnd', extention_after='doc',
+                       name_range=[3, 6])
+
 
 
 
